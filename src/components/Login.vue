8df15701b4dcaa040d14aefa47bd027062b96bb7
hollidavis <hollidavis19@gmail.com>
2021-08-06 15:23:14 -0600

    login page done

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login">
+    <!-- Profile Image & graduated status -->
     <div class="row my-5">
       <div class="w-100 d-flex justify-content-center align-items-center">
         <img class="profile-img round-border" :src="account.picture" :alt="account.name">
@@ -9,12 +10,26 @@
         <span class="p-3 bg-light round-border fa fa-user fa-2x" aria-hidden="true" v-else></span>
       </div>
     </div>
+    <!-- Profile Info -->
     <div class="row">
-      <div class="col">
+      <!-- Name & Class -->
+      <div class="col mx-3">
         <p class="text-gray m-0">
-          {{ account.class }} Tester
+          {{ account.class }}
         </p>
-        <h5>{{ account.name }}</h5>
+        <h4>{{ account.name }}</h4>
+      </div>
+      <!-- Links -->
+      <div class="col mt-3 d-flex flex-column mx-3 text-dark">
+        <p v-if="account.github" class="lead cursor" :href="account.github">
+          <span class="mr-2 fab fa-lg fa-github"></span><b> GitHub</b>
+        </p>
+        <p v-if="account.linkedin" class="lead cursor" :href="account.linkedin">
+          <span class="mr-2 fab fa-lg fa-linkedin"></span><b> LinkedIn</b>
+        </p>
+        <p v-if="account.resume" class="lead cursor" :href="account.resume">
+          <span class="mr-3 fa fa-lg fa-file-alt"></span><b>Resume</b>
+        </p>
       </div>
     </div>
   </div>
@@ -44,5 +59,8 @@ export default {
 .spill{
 margin-right: 3em;
 margin-top: -5em
+}
+.cursor{
+  cursor: pointer;
 }
 </style>
