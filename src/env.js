97a1442aa42a2e21a9b600cc938b411dd5d24e95
Holli Davis <hollidavis19@gmail.com>
2021-09-16 16:02:24 -0600

    env

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://bcw-sandbox.herokuapp.com/' : ''
-export const domain = 'codeworksclassroom.auth0.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
-export const audience = 'https://codeworksclassroom.com'
+export const domain = 'holli-codeworks.us.auth0.com'
+export const clientId = 'WOJPmg4INYdBncBTcsNC39o4sOap0MoP'
+export const audience = 'https://network.com'
